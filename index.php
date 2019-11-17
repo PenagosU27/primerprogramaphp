5956f1421017f148e50a8fad0b415c83eafae31b
Santiago Penagos Uribe <santipuribe@gmail.com>
2019-11-17 12:54:46 -0500

    Primer programa basico de html

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title> 
+    Hola Mundo 
+
+
+</title>
+</head> 
+<body>  
+    <!-- Esto es un comentario
+    -->
+     <h1>Hola mundo, me llamo Santiago</h1> 
+     <h2>Subtitulos</h2>
+     <h3>Contenido de informació</h3>
+     <h4>Un texto pequeño</h4>
+     <h5>Un texto más pequeño</h5>
+     <h6>Muy pequeño</h6> 
+     <p>Esto es un parrafo para brindar mucha informació</p> 
+     <p>Tengo un texto con estilo <b>Negrita</b></p>
+     <a href="http://www.google.com" target="_blank">Click aquí para ir a Google</a> 
+     <br> 
+     Hola Mundo 2. 
+     <br>
+     Este es otro texto 
+     <br> 
+     <a href="juego_the_witcher.php" <img src="thewitcher.jpg"> </a>
+</body>
+</html>
---
+++ juego_the_witcher.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title> The Witcher 
+    
+
+</title>  
+</head> 
+
+<body>  
+
+<h1> The Witcher Game </h1>  
+
+ <p> The Witcher (Wiedźmin en polaco) es una serie de videojuegos de RPG de acción y fantasía desarrollados por CD Projekt RED, basados en la saga de novelas de Geralt de Rivia, escritas por el autor polaco Andrzej Sapkowski. 
+</p>  
+<p> Basados en la saga de Geralt de Rivia, uno de los pocos brujos que quedan, asesinos de monstruos a sueldo con habilidades especiales. Los videojuegos cuentan las aventuras de Geralt a través de misiones que el jugador debe completar. 
+</p> 
+<p> The Witcher también tendrá una futura serie producida por Netflix que estrenará el 20 de diciembre de 2019.1​2​ Basada en las obras de la saga de Geralt de Rivia de Andrzej Sapkowski. La serie fue anunciada en 2017.
+</p>  
+
+  <img src="thewitcher1.jpg"> 
+  <a href="index.php">Volver</a>
+
+</body> 
+
+</html>
